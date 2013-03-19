999d858137e99c8d7404c28e8886bd1201247897
leandro-santos <leandrosantos@clickjogos.com>
2013-03-19 00:13:46 -0300

    implementado categorias (pagina principal)

--- app/controllers/feeds_controller.rb
+++ app/controllers/feeds_controller.rb
@@ -1,22 +1,30 @@
 class FeedsController < ApplicationController
 
   def index
+
+    @feeds = Feed.order('pub_date DESC NULLS LAST, updated_at DESC').limit(20)
+
     @feedsWorld = []
     @newsWorld = []
-    @feeds = []
-    Feed.order('pub_date DESC NULLS LAST, updated_at DESC').all().each do |feed|
-      @feeds << feed
+    @categories = []
+    @categoryFeeds = []
+    Category.all.each do |category|
+      @categories << category
+      @categoryFeeds << Feed.where('category_id = ?', category.id).limit(6)
       begin
-        if @feedsWorld.size <= 2
-          if !feed.news.first.title.blank? && feed.category_id == 8
-            @feedsWorld << feed
-            @newsWorld << feed.news.order('pub_date DESC NULLS LAST, created_at DESC').limit(4)
+        if category.id == 8
+          @categoryFeeds.last.each do |feed|
+            if @feedsWorld.size <= 2 && !feed.news.first.title.blank?
+              @feedsWorld << feed
+              @newsWorld << feed.news.order('pub_date DESC NULLS LAST, created_at DESC').limit(4)
+            end
           end
         end
       rescue => error
         p error
       end
     end
+
   end
 
 end
