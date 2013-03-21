0339c8d2a0cc159ae90c78aa1b321c6fae00ea40
leandro-santos <leandrosantos@clickjogos.com>
2013-03-21 10:36:09 -0300

    add adsense

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -4,7 +4,7 @@ class CategoriesController < ApplicationController
     @feeds = []
     @news = []
     @category = Category.find_by_slug(params[:id])
-    Feed.order('pub_date DESC NULLS LAST, updated_at DESC').find_all_by_category_id(@category.id).each do |i|
+    Feed.order('pub_date DESC NULLS LAST, updated_at DESC').includes(:category).find_all_by_category_id(@category.id).each do |i|
       @feeds << i
       @news << @feeds.last.news.order('pub_date DESC NULLS LAST, updated_at DESC').limit(6)
     end
@@ -15,7 +15,7 @@ class CategoriesController < ApplicationController
     @feed = Feed.find(params[:feed])
     @news = @feed.news.order('pub_date DESC NULLS LAST, updated_at ASC')
     category_id = Category.find_by_slug(params[:id]).id
-    Feed.order('pub_date DESC NULLS LAST, updated_at DESC').find_all_by_category_id(category_id).each do |i|
+    Feed.order('pub_date DESC NULLS LAST, updated_at DESC').includes(:category).find_all_by_category_id(category_id).each do |i|
       @feeds << i
     end
   end
